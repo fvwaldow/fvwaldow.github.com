fca6789c7ee324b0c389a95bd7bd7c0ad3597c86
fvwaldow <frederikvonwaldow@gmail.com>
2026-03-19 20:57:36 +0000

    Deploying to gh-pages from @ fvwaldow/fvwaldow.github.com@0aaeaba6a39ac31c1632e63696da62bb25e363d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-cv",
+          title: "CV",
+          description: "Curriculum Vitae",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
